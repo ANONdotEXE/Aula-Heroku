ec2f5920c28c7fee857869ea94210186f9c34251
Lucas <contatooliveiralucas@gmail.com>
2022-01-25 18:15:41 -0300

    teste04

--- index.php
+++ index.php
@@ -12,7 +12,63 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css">
-   
+   <style>
+       
+    font-size: 25px;
+    color: #fff;
+    padding: 10px;
+    display: inline;
+    font-family: 'Valorant';
+}
+
+#menu ul li a {
+    color: #fff;
+    padding: 20px;
+    display: inline-block;
+    text-decoration: none;
+    transition: background .1s;
+    font-family:'Quicksand', sans-serif ;
+}
+
+#menu ul li a:hover {
+    background: #1e2a54
+}
+
+#menu ul li:last-child a {
+    float: right;
+}
+
+#main {
+    position: absolute;
+    margin: 0;
+    height: 1280px;
+    width: 100%;
+    background: linear-gradient(-45deg, #0077b6, #1e2a54);
+}
+
+.info1{
+    padding-top: 60px;
+    padding-right: 130px;
+    float: right;
+}
+
+#texto1 {
+    color: #fff;
+    font-size: 1.75em;
+    padding-top: 100px;
+    padding-left: 130px;
+    float: left,
+}
+
+#texto2 { 
+    color: #fff;
+    font-size: 1.75em;
+    padding-left: 130px;
+    text-align:justify, left;
+}
+
+   </style>
+
 </head>
 
 <body>
