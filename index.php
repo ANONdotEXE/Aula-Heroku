c75e39534a8baed080d7ca552d509d7a83e44aea
Lucas <contatooliveiralucas@gmail.com>
2022-01-25 17:06:51 -0300

    teste6

--- index.php
+++ index.php
@@ -11,10 +11,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="" > 
 </head>
 
 <body>
-   
+<div>
+        <nav id="menu">
+            <ul>
+                <li>Lubank</li>
+                <li><a>Home</a></li>
+                <li><a>Cadastrar</a></li>
+                <li><a>href="#">Sobre nós</a></li>
+                <li><a>Contato</a></li>
+                <li><a>Entrar</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div id="main">
+        <div class="info1">
+            <img src="Wallpaper.png" width="284" height="479">
+        </div>
+        <div id="texto1">
+            <h1>A Conta do Lubank</h1>
+        </div>
+        <br>
+        <br>
+        <div id="texto2">
+            <h2>A conta que vai levar você <br>ao controle da sua vida <br> financeira</h2>
+        </div>
+    </div>
 </body>
 
 </html>
